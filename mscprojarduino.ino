a01099c1741ff2b8813f8cc37d484dabafd49f58
SussexMscProject <68085146+SussexMscProject@users.noreply.github.com>
2020-07-11 00:01:11 +0100

    Update mscprojarduino.ino
    varing displayed values and introduce signal generator screen.

--- mscprojarduino.ino
+++ mscprojarduino.ino
@@ -1,3 +1,4 @@
+#include <MemoryFree.h>
 #include <Elegoo_GFX.h>    // Core graphics library
 #include <Elegoo_TFTLCD.h> // Hardware-specific library
 #define LCD_CS A3 // Chip Select goes to Analog 3
@@ -16,12 +17,7 @@
 
 const int b1 = 36;
 const int b2 = 50;
-const int b3 = 48;
-const int b4 = 46;
-const int b5 = 44;
-const int b6 = 42;
-const int b7 = 40;
-const int b8 = 38;
+const int b3 = 38;
 const int f1 = 34;
 const int f2 = 30;
 
@@ -31,11 +27,6 @@ void setup(void) {
   pinMode(b1, INPUT);
   pinMode(b2, INPUT);
   pinMode(b3, INPUT);
-  pinMode(b4, INPUT);
-  pinMode(b5, INPUT);
-  pinMode(b6, INPUT);
-  pinMode(b7, INPUT);
-  pinMode(b8, INPUT);
   pinMode(f1, INPUT);
   pinMode(f2, OUTPUT);
   Serial.begin(115200);
@@ -44,36 +35,53 @@ void setup(void) {
   tft.reset();
   tft.begin(37697);
   tft.setRotation(1);
-  tft.fillScreen(BLACK);
-  draw_text();
 }
 
 const uint16_t width = 320;
 const uint8_t height = 240;
 
 void loop(void){
-  draw_grid();
   if(digitalRead(b1)==HIGH)
     print_wave();
+  if(digitalRead(b3)==HIGH)
+    Signal_gen();
   Serial.println("im in the main loop");
 }
 
+void Signal_gen(){
+
+  tft.setTextSize(3);
+  tft.setCursor(0, 0);
+  tft.fillScreen(BLACK);
+  tft.setTextColor(WHITE);
+  tft.println("Generating Signal");
+  while(digitalRead(b3)==HIGH){
+  }
+  tft.setCursor(0, 0);
+  tft.setTextColor(BLACK);  
+  tft.println("Generating Signal:");
+}
+uint8_t pixel_buffer[320][2];
 void print_wave(){
-  
   uint8_t vmin = 255;
+  uint8_t vmin_ref = 255;
   uint8_t vmax = 0;
-  uint8_t pixel_buffer[320][2];
+  uint8_t vmax_ref;
   uint16_t i = 0;
   uint8_t threshold = 0;
   uint16_t crossed = 0;
   uint8_t sample_split = 0;
   uint16_t volt_split = 0;
-  uint16_t volt_split_ref = volt_split;
+  uint16_t volt_split_ref = 0;
   uint32_t frequency = 0;
-  uint32_t frequency_ref = frequency;
+  uint32_t frequency_ref = 0;
   uint16_t time_div = 0;
   uint16_t time_div_ref = 0;
+  uint32_t volt_div_ref = 0;
+  uint32_t volt_div = 0;
   tft.setTextSize(1);
+  tft.fillScreen(BLACK);
+  draw_text();
   Serial1.flush();
   while(digitalRead(b1)==HIGH){
     i = 0;
@@ -106,6 +114,7 @@ void print_wave(){
       i++;
     }
     i = 0;
+    
     sample_split = pixel_buffer[318][2];
     volt_split = pixel_buffer[319][2];
      
@@ -126,42 +135,36 @@ void print_wave(){
       update_text(100, 0, time_div_ref, time_div);
       time_div_ref = time_div;
     }
+
+    volt_div = (3300/6)*(100.0/float(volt_split));
+    if(volt_div!=volt_div_ref){
+      update_text(100, 10, volt_div_ref, volt_div);
+      volt_div_ref = volt_div;
+    }
     
-    //frequency = (((10*sample_split*320)/(crossed*1000000)));
-    Serial.println((((10*sample_split*320)/(crossed*1000000))));
-//    if(frequency!=frequency_ref){
-//      update_text(100, 20, time_div_ref, short(frequency));
-//      frequency_ref = frequency;
-//    }
-    //Serial.println(frequency);
-    //canary();
+    frequency = (1000000L*crossed)/(10L*sample_split*32);
+    if(frequency!=frequency_ref){
+      update_text(100, 20, frequency_ref, frequency);
+      frequency_ref = frequency;
+    }
     
+    if(vmax!=vmax_ref){
+      update_text(100, 30, (long(vmax_ref)*1000L)/3300L, (long(vmax)*1000L)/3300L);
+      vmax_ref = vmax;
+    }
   }
 }
 
-void canary(){
-    Serial.print(b2);
-    Serial.print(" ");
-    Serial.print(b3);
-    Serial.print(" ");
-    Serial.print(b4);
-    Serial.print(" ");
-    Serial.print(b5);
-    Serial.print(" ");
-    Serial.print(b6);
-    Serial.print(" ");
-    Serial.println(b7);
+void update_text(uint32_t x, uint32_t y, uint32_t ref, uint32_t val){
   
+  tft.setCursor(x, y);
+  tft.setTextColor(BLACK);
+  tft.println(ref);
+  tft.setTextColor(WHITE);  
+  tft.setCursor(x, y);
+  tft.println(val);
 }
 
-void update_text(uint8_t x, uint8_t y, uint16_t ref, uint16_t val){
-    tft.setCursor(x, y);
-    tft.setTextColor(BLACK);
-    tft.println(ref);
-    tft.setTextColor(WHITE);  
-    tft.setCursor(x, y);
-    tft.println(val);
-}
 
 void draw_text(){
   tft.setTextColor(WHITE);  
@@ -170,10 +173,10 @@ void draw_text(){
   tft.println("Time div/us");
   tft.setCursor(0, 10);  
   tft.setTextSize(1);
-  tft.println("Volt div");
+  tft.println("Volt div/mV");
   tft.setCursor(0, 20);  
   tft.setTextSize(1);
-  tft.println("Frequency");
+  tft.println("Frequency/Hz");
   tft.setCursor(0, 30);  
   tft.setTextSize(1);
   tft.println("Volt max");
