ed03e7ae9bef744257eba4426e78c251f7334ef2
SussexMscProject <68085146+SussexMscProject@users.noreply.github.com>
2020-08-07 23:05:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "mbed.h"
 
 Timer test;
+DigitalOut test_R(D15);
+DigitalOut test_C(D14);
 
 int main() {
     initbt();
@@ -16,7 +18,16 @@ int main() {
     uint8_t multiplier = 0;
     uint32_t change = 0;
 
-    while(command==0){
+    
+    IWDG_HandleTypeDef hiwdg;
+    
+    hiwdg.Instance = IWDG;
+    hiwdg.Init.Prescaler = IWDG_PRESCALER_256; 
+    hiwdg.Init.Reload = 1000;  
+    //watchdog refresh rate at (256*1000)/(32*10^3)  = 8 seconds   
+
+    while(command==0){         
+        HAL_IWDG_Init(&hiwdg);
         poll();
     }
     printf("%d\r\n",(command>>24)&0xFF);
@@ -33,7 +44,18 @@ int main() {
             Signal_Gen();
         if(((command>>24)&0xFF)==2){
             Read_Send_Signal();}
-
+        while(((poll()>>24)&0xFF)==3){
+            HAL_IWDG_Init(&hiwdg);
+            test_R = 1;
+        }
+        if(test_R == 1)
+            test_R = 0;
+        while(((poll()>>24)&0xFF)==4){          
+            HAL_IWDG_Init(&hiwdg);
+            test_C = 1;
+        }
+        if(test_C == 1)          
+            test_C = 0;
+        
     }
 }
-
